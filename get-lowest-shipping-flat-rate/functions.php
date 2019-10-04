9fd50e679ad7c04d87ffa854e97c85fd90b59138
Eyüp Gülşen <40281221+MrEbabi@users.noreply.github.com>
2019-10-04 17:07:46 +0300

    solution updated.
    zone name is added to solution.

--- get-lowest-shipping-flat-rate/functions.php
+++ get-lowest-shipping-flat-rate/functions.php
@@ -4,16 +4,18 @@ function get_lowest_shipping_flat_rate_1()
 
     //define the array outside of the loop
     $shipping_costs = [];
+    $min_zone = "";
 
     //get all costs in a loop and store them in the array
     foreach ((array) $delivery_zones as $key => $the_zone ) {
 
     foreach ($the_zone['shipping_methods'] as $value) {
         $shipping_costs[] = $value->cost;
+        if(min($shipping_costs) == $value->cost) $min_zone = $the_zone['zone_name'];
         }
     }
     
-    $content = get_woocommerce_currency_symbol() . " " . min($shipping_costs);
+    $content = $min_zone . " - " . get_woocommerce_currency_symbol() . " " . min($shipping_costs);
     
     return $content;
 }
@@ -26,18 +28,23 @@ function get_lowest_shipping_flat_rate_2()
 
     //define the array outside of the loop
     $shipping_costs = [];
+    $min_zone = "";
 
     //get all costs in a loop and store them in the array
     foreach ((array) $delivery_zones as $key => $the_zone ) {
 
     foreach ($the_zone['shipping_methods'] as $value) {
         $shipping_costs[] = $value->cost;
+        if(min($shipping_costs) == $value->cost) $min_zone = $the_zone['zone_name'];
         }
     }
-
+    
+    echo $min_zone . " - Rate: ";
+    
     //display currency symbol if you want
     echo get_woocommerce_currency_symbol();
 
     //after the loop, print the minimum item in the array
     echo (min($shipping_costs));
+
 }
