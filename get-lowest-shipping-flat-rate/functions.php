41ccef0a14bdd4676f901cf80d76cb5449e6297d
Eyüp Gülşen <40281221+MrEbabi@users.noreply.github.com>
2019-10-04 15:50:08 +0300

    solution.

---
+++ get-lowest-shipping-flat-rate/functions.php
@@ -0,0 +1,45 @@
+function get_lowest_shipping_flat_rate_1()
+{
+    $delivery_zones = WC_Shipping_Zones::get_zones(); 
+
+    //define the array outside of the loop
+    $shipping_costs = [];
+
+    //get all costs in a loop and store them in the array
+    foreach ((array) $delivery_zones as $key => $the_zone ) {
+
+    foreach ($the_zone['shipping_methods'] as $value) {
+        $shipping_costs[] = $value->cost;
+        break;
+        }
+    }
+    
+    $content = get_woocommerce_currency_symbol() . " " . min($shipping_costs);
+    
+    return $content;
+}
+
+//or
+
+function get_lowest_shipping_flat_rate_2()
+{
+    $delivery_zones = WC_Shipping_Zones::get_zones(); 
+
+    //define the array outside of the loop
+    $shipping_costs = [];
+
+    //get all costs in a loop and store them in the array
+    foreach ((array) $delivery_zones as $key => $the_zone ) {
+
+    foreach ($the_zone['shipping_methods'] as $value) {
+        $shipping_costs[] = $value->cost;
+        break;
+        }
+    }
+
+    //display currency symbol if you want
+    echo get_woocommerce_currency_symbol();
+
+    //after the loop, print the minimum item in the array
+    echo (min($shipping_costs));
+}
